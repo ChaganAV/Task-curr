f99c1a52507085008a577a796ec6699c02484ac5
Alexandr <jwchaganav@gmail.com>
2022-10-31 13:20:06 +0400

    Список валют

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("=== Конвертер валют ===");
+
+void SelectListCurrent()
+{
+    Console.WriteLine("Список вариантов валют");
+    Console.WriteLine("USD - Доллар США");
+    Console.WriteLine("EUR - Евро");
+    Console.WriteLine("CBP - Фунт стерлинг Соединенного Королевства");
+    Console.WriteLine("CHF - Швейцарский франк");
+    Console.WriteLine("JPY - Японская иена");
+    Console.WriteLine("RUB - Российский рубль");
+}
